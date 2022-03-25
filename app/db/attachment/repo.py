d6842df4c788cd5817b00f4db4f259f3b6c078ea
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-03-25 10:54:27 +0300

    Remove file storage from repo

--- app/db/attachment/repo.py
+++ app/db/attachment/repo.py
@@ -1,24 +1,23 @@
+from uuid import UUID
+
 from aiofiles.tempfile.temptypes import AsyncSpooledTemporaryFile
 
 from app.db.attachment.models import AttachmentModel
 from app.db.crud import CRUD
 from app.db.sqlalchemy import AsyncSession
 from app.schemas.attachments import Attachment
-from app.services.file_storage import FileStorage
 
 
 class AttachmentRepo:
     
-    def __init__(self, file_storage: FileStorage, session: AsyncSession):
+    def __init__(self, session: AsyncSession):
         self._crud = CRUD(session=session, cls_model=AttachmentModel)
-        self._file_storage = file_storage
 
     async def create_attachment(
         self, 
-        file: AsyncSpooledTemporaryFile, 
+        file_storage_id: UUID, 
         filename: str
     ) -> Attachment:
-        file_storage_id = await self._file_storage.save(file)
         
         row = await self._crud.create(
             model_data={"file_storage_id": file_storage_id, "filename": filename},
