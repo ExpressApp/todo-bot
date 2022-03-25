724d01f9eee25e2cc82a44481fbfe4a8b0cf5f61
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-03-25 14:44:17 +0300

    Make migrations

--- app/db/migrations/env.py
+++ app/db/migrations/env.py
@@ -13,8 +13,9 @@ from app.settings import settings  # isort:skip
 from app.db.sqlalchemy import Base, make_url_sync  # isort:skip
 
 # Import models to make them visible by alembic
-import app.db.record.models  # isort:skip
 import app.db.attachment.models
+import app.db.record.models  # isort:skip
+import app.db.task.models
 
 postgres_dsn = make_url_sync(settings.POSTGRES_DSN)
 context_config = context.config
