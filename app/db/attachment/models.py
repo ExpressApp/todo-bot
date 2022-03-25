7558fbf04204ac7fbb3bc57a912de6ba29d35ecb
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-03-25 14:43:26 +0300

    Update AttachmentModel

--- app/db/attachment/models.py
+++ app/db/attachment/models.py
@@ -1,15 +1,21 @@
 from uuid import UUID
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import relationship
 
 from app.db.sqlalchemy import Base
 
 
 class AttachmentModel(Base):
-    __tablename__ = "tasks"
+    __tablename__ = "attachments"
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    id: int = Column(Integer, ForeignKey('tasks.attachment_id'), primary_key=True, autoincrement=True)
     file_storage_uuid: UUID = Column(postgresql.UUID(as_uuid=True))
     filename: str = Column(String)
+
+    task = relationship(
+        "app.db.task.models.TaskModel",
+        back_populates="attachments"
+    )
     
