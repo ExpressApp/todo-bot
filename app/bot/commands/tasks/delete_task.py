5daed3b4eb62af2a6cb8800941eb7b497c32aa29
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-04-06 16:52:02 +0300

    Tasks deleting via relations

--- app/bot/commands/tasks/delete_task.py
+++ app/bot/commands/tasks/delete_task.py
@@ -4,7 +4,6 @@ from botx import Bot, BubbleMarkup, HandlerCollector, IncomingMessage
 
 from app.bot import constants
 from app.bot.middlewares.db_session import db_session_middleware
-from app.db.attachment.repo import AttachmentRepo
 from app.db.task.repo import TaskRepo
 from app.services.answers.status import get_status_message
 from app.services.file_storage import FileStorage
@@ -24,7 +23,6 @@ async def delete_task(message: IncomingMessage, bot: Bot) -> None:
 
     db_session = message.state.db_session
 
-    attachment_repo = AttachmentRepo(db_session)
     task_repo = TaskRepo(message.state.db_session)
 
     task_id = message.data["task_id"]
@@ -32,10 +30,8 @@ async def delete_task(message: IncomingMessage, bot: Bot) -> None:
     task = await task_repo.get_task(task_id)
     await task_repo.delete_task(task_id)
 
-    if task.attachment_id:
-        attachment = await attachment_repo.get_attachment(task.attachment_id)
-        await attachment_repo.remove_attachment(attachment.id)
-        await file_storage.remove(attachment.file_storage_id)
+    if task.attachment:
+        await file_storage.remove(task.attachment.file_storage_id)
     
     await db_session.commit()
 
