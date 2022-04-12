20083e736bf97c663bf4b68a3471240136644ed6
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-04-12 17:38:04 +0300

    Add tests for contact handler

--- tests/test_commands/test_create_task.py
+++ tests/test_commands/test_create_task.py
@@ -1,7 +1,8 @@
 import asyncio
 from typing import Awaitable, Callable
+from uuid import uuid4
 
-from botx import AttachmentTypes, Bot, BubbleMarkup, Button, IncomingMessage, KeyboardMarkup, lifespan_wrapper
+from botx import AttachmentTypes, Bot, BubbleMarkup, Button, IncomingMessage, KeyboardMarkup, Mention, MentionList, lifespan_wrapper
 from botx.models.attachments import AttachmentDocument
 from botx.models.commands import BotCommand
 import pytest
@@ -152,6 +153,128 @@ async def test_description_handler(
     # - Assert -
     bot.answer_message.assert_awaited_with(
         body="При необходимости отметьте **одного коллегу**, связанного с задачей, через `@@`:",
+        bubbles=BubbleMarkup(
+            [
+                [Button(command="SKIP", label="Пропустить")],
+            ],
+        ),
+        keyboard=KeyboardMarkup(
+            [
+                [Button(command="CANCEL", label="Отменить")],
+            ]
+        ),
+    )
+    
+
+async def test_wrong_contact(
+    bot: Bot,
+    incoming_message_factory: Callable[..., IncomingMessage],
+) -> None:
+    # - Arrange -
+    command_message = incoming_message_factory(body="/создать")
+    title_message = incoming_message_factory(body="Title")
+    description_message = incoming_message_factory(body="Description")
+    foo_mesage = incoming_message_factory(body="foo")
+
+    # - Act -
+    async with lifespan_wrapper(bot):
+        bot.async_execute_bot_command(command_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(title_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(description_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(foo_mesage)
+        await asyncio.sleep(1)
+        await title_message.state.fsm.drop_state()
+
+    # - Assert -
+    bot.answer_message.assert_awaited_with(
+        body="Вы некорректно отметили коллегу. Пожалуйста, укажите **только одного** пользователя **через @@**",
+        bubbles=BubbleMarkup(
+            [
+                [Button(command="SKIP", label="Пропустить")],
+            ],
+        ),
+        keyboard=KeyboardMarkup(
+            [
+                [Button(command="CANCEL", label="Отменить")],
+            ]
+        ),
+    )
+
+async def test_contact_handler(
+    bot: Bot,
+    incoming_message_factory: Callable[..., IncomingMessage],
+) -> None:
+    # - Arrange -
+    command_message = incoming_message_factory(body="/создать")
+    title_message = incoming_message_factory(body="Title")
+    description_message = incoming_message_factory(body="Description")
+    
+    contact = Mention.contact(huid=uuid4())
+    contact_message = incoming_message_factory(body=str(contact))
+    contact_message.mentions = MentionList([contact])
+
+    # - Act -
+    async with lifespan_wrapper(bot):
+        bot.async_execute_bot_command(command_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(title_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(description_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(contact_message)
+        await asyncio.sleep(1)
+        await title_message.state.fsm.drop_state()
+
+    # - Assert -
+    bot.answer_message.assert_awaited_with(
+        body="Прикрепите **вложение** (опционально):",
+        bubbles=BubbleMarkup(
+            [
+                [Button(command="SKIP", label="Пропустить")],
+            ],
+        ),
+        keyboard=KeyboardMarkup(
+            [
+                [Button(command="CANCEL", label="Отменить")],
+            ]
+        ),
+    )
+
+
+async def test_not_one_contact(
+    bot: Bot,
+    incoming_message_factory: Callable[..., IncomingMessage],
+) -> None:
+    # - Arrange -
+    command_message = incoming_message_factory(body="/создать")
+    title_message = incoming_message_factory(body="Title")
+    description_message = incoming_message_factory(body="Description")
+    
+    first_contact = Mention.contact(huid=uuid4())
+    second_contact = Mention.contact(huid=uuid4())
+    contact_message = incoming_message_factory(
+        body=" ".join([str(first_contact), str(second_contact)])
+    )
+    contact_message.mentions = MentionList([first_contact, second_contact])
+
+    # - Act -
+    async with lifespan_wrapper(bot):
+        bot.async_execute_bot_command(command_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(title_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(description_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(contact_message)
+        await asyncio.sleep(1)
+        await title_message.state.fsm.drop_state()
+
+    # - Assert -
+    bot.answer_message.assert_awaited_with(
+        body="Вы некорректно отметили коллегу. Пожалуйста, укажите **только одного** пользователя **через @@**",
         bubbles=BubbleMarkup(
             [
                 [Button(command="SKIP", label="Пропустить")],
