0c05403f208d2720763265968af6a09aa57a834f
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-04-12 16:47:48 +0300

    Add tests for description handler

--- tests/test_commands/test_create_task.py
+++ tests/test_commands/test_create_task.py
@@ -4,6 +4,18 @@ from typing import Awaitable, Callable
 from botx import AttachmentTypes, Bot, BubbleMarkup, Button, IncomingMessage, KeyboardMarkup, lifespan_wrapper
 from botx.models.attachments import AttachmentDocument
 from botx.models.commands import BotCommand
+import pytest
+
+
+@pytest.fixture
+def incoming_attachment() -> AttachmentDocument:
+    return AttachmentDocument(
+        type=AttachmentTypes.DOCUMENT,
+        filename="test.txt",
+        size=len(b"Hello, world!\n"),
+        is_async_file=False,
+        content=b"Hello, world!\n",
+    )
 
 
 async def test_create_task_handler(
@@ -31,18 +43,13 @@ async def test_create_task_handler(
 async def test_file_instead_title(
     bot: Bot,
     incoming_message_factory: Callable[..., IncomingMessage],
+    incoming_attachment: AttachmentDocument
 ) -> None:
     # - Arrange -
     command_message = incoming_message_factory(body="/создать")
     
     file_message = incoming_message_factory()
-    file_message.file = AttachmentDocument(
-        type=AttachmentTypes.DOCUMENT,
-        filename="test.txt",
-        size=len(b"Hello, world!\n"),
-        is_async_file=False,
-        content=b"Hello, world!\n",
-    )
+    file_message.file = incoming_attachment
 
     # - Act -
     async with lifespan_wrapper(bot):
@@ -89,3 +96,71 @@ async def test_task_title_handler(
             ]
         ),
     )
+
+async def test_file_instead_description(
+    bot: Bot,
+    incoming_message_factory: Callable[..., IncomingMessage],
+    incoming_attachment: AttachmentDocument,
+) -> None:
+    # - Arrange -
+    command_message = incoming_message_factory(body="/создать")
+    title_message = incoming_message_factory(body="Title")
+
+    file_message = incoming_message_factory()
+    file_message.file = incoming_attachment
+
+    # - Act -
+    async with lifespan_wrapper(bot):
+        bot.async_execute_bot_command(command_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(title_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(file_message)
+        await asyncio.sleep(1)
+        await title_message.state.fsm.drop_state()
+
+    # - Assert -
+    bot.answer_message.assert_awaited_with(
+        body="Чтобы указать **описание** задачи, введите его **текстом**",
+        keyboard=KeyboardMarkup(
+            [
+                [Button(command="CANCEL", label="Отменить")],
+            ]
+        ),
+    )
+
+
+async def test_description_handler(
+    bot: Bot,
+    incoming_message_factory: Callable[..., IncomingMessage],
+) -> None:
+    # - Arrange -
+    command_message = incoming_message_factory(body="/создать")
+    title_message = incoming_message_factory(body="Title")
+    description_message = incoming_message_factory(body="Description")
+
+    # - Act -
+    async with lifespan_wrapper(bot):
+        bot.async_execute_bot_command(command_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(title_message)
+        await asyncio.sleep(1)
+        bot.async_execute_bot_command(description_message)
+        await asyncio.sleep(1)
+        await title_message.state.fsm.drop_state()
+
+    # - Assert -
+    bot.answer_message.assert_awaited_with(
+        body="При необходимости отметьте **одного коллегу**, связанного с задачей, через `@@`:",
+        bubbles=BubbleMarkup(
+            [
+                [Button(command="SKIP", label="Пропустить")],
+            ],
+        ),
+        keyboard=KeyboardMarkup(
+            [
+                [Button(command="CANCEL", label="Отменить")],
+            ]
+        ),
+    )
+    
